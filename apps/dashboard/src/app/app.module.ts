4dcc922f1f2cbb07e0d6413d9c77089d3d852b7b
Dima Simonov <keldor80@gmail.com>
2019-02-11 16:24:18 +0200

    project setup

---
+++ apps/dashboard/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { NxModule } from '@nrwl/nx';
+import { MaterialModule } from '@workshop/material';
+
+import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { ProjectsModule } from './projects/projects.module';
+import { CustomersModule } from './customers/customers.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    NxModule.forRoot(),
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    BrowserAnimationsModule,
+    MaterialModule,
+    HomeModule,
+    ProjectsModule,
+    CustomersModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
